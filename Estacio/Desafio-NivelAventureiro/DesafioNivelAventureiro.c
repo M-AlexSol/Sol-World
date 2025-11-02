b38b280a6f902e956e8cd38ae43a2e57b33191d9
Matheus <math.alexsol@gmail.com>
2025-11-02 12:04:33 -0300

    Mudança de nome dos desafios e construção do Desafio Nivel Aventureiro

--- Estacio/Desafio-NivelAventureiro/DesafioNivelAventureiro.c
+++ Estacio/Desafio-NivelAventureiro/DesafioNivelAventureiro.c
@@ -1,16 +1,10 @@
 /*
 Objetivo da Atividade:
-    Criar duas cartas do Super Trunfo, sem utilizar while/for/if/else, apenas com o que foi aprendido.
-    Pela solicitacao da atividade, sem reducao de casa decimal no float tambem.
-
+   Após criar as duas cartas, precisamos calcular a Densidade Populacional e 
+   PIB per Capita
 Dos dados:
-    Estado: Escolher da letra A ao H (char)
-    Codigo da Carta: Escolher do 01 ao 04 (string - char[5])
-    Cidade: Informar o nome da cidade (string - cha[50])
-    Populacao: Quantidade de pessoas da cidade (int)
-    Pontos Turisticos: Quantidade de pontos turisticos da cidade (int)
-    Area: Informar a area, em km quadrados, da cidade (float)
-    PIB: produto interno bruto da cidade (float)
+    Densidade Populacional = População/Área (float) "hab/km²"
+    PIB per Capita = PIB/População (float) "reais"
 
 Apos inserir os valores, passar no final de forma legivel os dados de cada carta, entitulando de
 Carta 1 e Carta 2.
@@ -24,6 +18,8 @@ Carta 1 e Carta 2.
         Área: 1521.11 km²
         PIB: 699.28 bilhões de reais
         Número de Pontos Turísticos: 50
+        Densidade Populacional: 52260.50 hab/km²
+        PIB per Capita: 120045.86 reais
 */
 
 #include <stdio.h>
@@ -31,7 +27,7 @@ Carta 1 e Carta 2.
 int main(){
     char estado1,estado2,cod1[5],cod2[5],cidade1[50],cidade2[50];
     int pop1,pop2,pturis1,pturis2;
-    float area1,area2,pib1,pib2;
+    float area1,area2,pib1,pib2,denspop1,denspop2,pibcap1,pibcap2;
 
     //Dados das Cartas 1 e 2
     printf(">>Dados da Carta 1<<\n");
@@ -66,9 +62,17 @@ int main(){
     printf("Informe a quantidade de Pontos Turisticos da cidade: ");
     scanf("%i",&pturis2);
 
+    /*Cálculos da Densidade Populacional e PIB per Capita das cartas
+    Feita conversão de bilhão para real ao multiplicar por 1000000000
+    */
+    denspop1=(float)pop1/area1;
+    denspop2=(float)pop2/area2;
+    pibcap1=(float)pib1*1000000000/pop1;
+    pibcap2=(float)pib2*1000000000/pop2;
+
     //Impressão das Cartas 1 e 2
-    printf("\nCarta 1:\nEstado: %c\nCódigo: %c%s\nNome da Cidade: %s\nPopulação: %i\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turisticos: %i\n\n", estado1,estado1,cod1,cidade1,pop1,area1,pib1,pturis1);
-    printf("Carta 2:\nEstado: %c\nCódigo: %c%s\nNome da Cidade: %s\nPopulação: %i\nÁrea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turisticos: %i\n", estado2,estado2,cod2,cidade2,pop2,area2,pib2,pturis2);
+    printf("\nCarta 1:\nEstado: %c\nCódigo: %c%s\nNome da Cidade: %s\nPopulação: %i\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turisticos: %i\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n\n", estado1,estado1,cod1,cidade1,pop1,area1,pib1,pturis1,denspop1,pibcap1);
+    printf("Carta 2:\nEstado: %c\nCódigo: %c%s\nNome da Cidade: %s\nPopulação: %i\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turisticos: %i\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n", estado2,estado2,cod2,cidade2,pop2,area2,pib2,pturis2,denspop2,pibcap2);
 
     return 0;
 }
